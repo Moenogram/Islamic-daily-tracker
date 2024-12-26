6a3f44503f5822e6479c41eae7cc3ec12508e8c2
Moenogram <100244787+Moenogram@users.noreply.github.com>
2024-12-26 15:56:03 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
+'use client';
+
+import './globals.css';
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="de">
-      <body>{children}</body>
+      <body className="bg-slate-900">{children}</body>
     </html>
-  )
+  );
 }
